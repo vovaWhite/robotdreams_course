4774f1bf23cd531d17231f9d1ebbac3e1734c85c
Volodymyr Butynets <volodymyr.butynets@gmail.com>
2025-09-25 17:33:27 +0300

    Completed lesson3 tasks

---
+++ lesson3/src/logical.js
@@ -0,0 +1,19 @@
+const first = '10';
+const second = 'false';
+const third = 13;
+const fifth = [0, 2, 5];
+const sixth = [7, 3, 254];
+const seventh = true;
+const eighth = false;
+const ninth = 'true';
+console.log('***************************');
+
+console.log(first > third);
+console.log(first < third);
+console.log(second == eighth);
+console.log(second === eighth);
+console.log(seventh == ninth);
+console.log(fifth < sixth);
+console.log(second > first);
+console.log(second > first);
+console.log(second && eighth);
