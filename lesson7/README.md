25f7e8ab23d70879102eb6b00f808c040d42878f
Volodymyr Butynets <volodymyr.butynets@gmail.com>
2025-10-13 17:54:03 +0300

    Completed lesson7 homework

---
+++ lesson7/README.md
@@ -0,0 +1,1 @@
+This is homework for lesson7
---
+++ lesson7/src/functions.ts
@@ -0,0 +1,13 @@
+function arrSum (array: (number | string)[]): number {
+    let total = 0;
+    for (const i of array)
+        total += Number(i);
+    return total;
+}
+
+
+const firstArray1 = [15, 2, 3];
+const secondArray2 = ['1', '2', '4'];
+
+console.log(arrSum(firstArray1));
+console.log(arrSum(secondArray2));
